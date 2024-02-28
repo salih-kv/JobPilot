91f41f7e92636a97c10625ae06b49f9d503c6d7a
salih-kv <profileofsalih@gmail.com>
2024-02-28 22:20:54 +0530

    add: header & welcome component

---
+++ components/common/header/ScreenHeaderBtn.jsx
@@ -0,0 +1,16 @@
+import { Image, Pressable } from "react-native";
+import styles from "./screenheader.style";
+
+const ScreenHeaderBtn = ({ iconUrl, dimension, handlePress }) => {
+  return (
+    <Pressable style={styles.btnContainer} onPress={handlePress}>
+      <Image
+        source={iconUrl}
+        resizeMode="cover"
+        style={styles.btnImg(dimension)}
+      />
+    </Pressable>
+  );
+};
+
+export default ScreenHeaderBtn;
