ec812957a444449a26634c3869e98f94929858b9
salih-kv <profileofsalih@gmail.com>
2024-02-29 17:53:41 +0530

    Implement search functionality

---
+++ app/search/[id].js
@@ -0,0 +1,142 @@
+import { useEffect, useState } from "react";
+import { Stack, useLocalSearchParams, useRouter } from "expo-router";
+import {
+  ActivityIndicator,
+  FlatList,
+  Image,
+  Pressable,
+  SafeAreaView,
+  Text,
+  View,
+} from "react-native";
+import axios from "axios";
+
+import styles from "../../styles/search";
+import { COLORS, SIZES, icons } from "../../constants";
+import { NearbyJobCard, ScreenHeaderBtn } from "../../components";
+
+const JobSearch = () => {
+  const params = useLocalSearchParams();
+  const router = useRouter();
+  const [searchResult, setSearchResult] = useState([]);
+  const [searchLoader, setSearchLoader] = useState(false);
+  const [searchError, setSearchError] = useState(null);
+  const [page, setPage] = useState(1);
+
+  const handleSearch = async () => {
+    setSearchLoader(true);
+    setSearchResult([]);
+
+    try {
+      const options = {
+        method: "GET",
+        url: `https://jsearch.p.rapidapi.com/search`,
+        headers: {
+          "X-RapidAPI-Key":
+            "1608be1040mshade075e2c5dcd1ep1a33c4jsn08c01b3b1f2e",
+          "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
+        },
+        params: {
+          query: params.id,
+          page: page.toString(),
+        },
+      };
+
+      const res = await axios.request(options);
+      setSearchResult(res.data.data);
+    } catch (e) {
+      setSearchError(e);
+      console.log(e);
+    } finally {
+      setSearchLoader(false);
+    }
+  };
+
+  const handlePagination = (direction) => {
+    if (direction === "left" && page > 1) {
+      setPage(page - 1);
+      handleSearch();
+    } else if (direction === "right") {
+      setPage(page + 1);
+      handleSearch();
+    }
+  };
+
+  useEffect(() => {
+    handleSearch();
+  }, []);
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Stack.Screen
+        options={{
+          headerStyle: { backgroundColor: COLORS.lightWhite },
+          headerShadowVisible: false,
+          headerLeft: () => (
+            <ScreenHeaderBtn
+              iconUrl={icons.left}
+              dimension="60%"
+              handlePress={() => router.back()}
+            />
+          ),
+          headerTitle: "",
+        }}
+      />
+
+      <FlatList
+        data={searchResult}
+        renderItem={({ item }) => (
+          <NearbyJobCard
+            job={item}
+            handleNavigate={() => router.push(`/job-details/${item.job_id}`)}
+          />
+        )}
+        keyExtractor={(item) => item.job_id}
+        contentContainerStyle={{ padding: SIZES.medium, rowGap: SIZES.medium }}
+        ListHeaderComponent={() => (
+          <>
+            <View style={styles.container}>
+              <Text style={styles.searchTitle}>{params.id}</Text>
+              <Text style={styles.noOfSearchedJobs}>Job Opportunities</Text>
+            </View>
+            <View style={styles.loaderContainer}>
+              {searchLoader ? (
+                <ActivityIndicator size="large" color={COLORS.primary} />
+              ) : (
+                searchError && <Text>Oops something went wrong</Text>
+              )}
+            </View>
+          </>
+        )}
+        ListFooterComponent={() => (
+          <View style={styles.footerContainer}>
+            <Pressable
+              style={styles.paginationButton}
+              onPress={() => handlePagination("left")}
+            >
+              <Image
+                source={icons.chevronLeft}
+                style={styles.paginationImage}
+                resizeMode="contain"
+              />
+            </Pressable>
+            <View style={styles.paginationTextBox}>
+              <Text style={styles.paginationText}>{page}</Text>
+            </View>
+            <Pressable
+              style={styles.paginationButton}
+              onPress={() => handlePagination("right")}
+            >
+              <Image
+                source={icons.chevronRight}
+                style={styles.paginationImage}
+                resizeMode="contain"
+              />
+            </Pressable>
+          </View>
+        )}
+      />
+    </SafeAreaView>
+  );
+};
+
+export default JobSearch;
